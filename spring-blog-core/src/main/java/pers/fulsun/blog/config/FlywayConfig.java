84b06b8257605ad3d3d8b646df05b492c6b87f9d
fulsun <fl.yi6145@gmail.com>
2024-12-22 01:38:13 +0800

    新增Flyway配置类和相关数据库初始化脚本

---
+++ spring-blog-core/src/main/java/pers/fulsun/blog/config/FlywayConfig.java
@@ -0,0 +1,41 @@
+package pers.fulsun.blog.config;
+
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.FlywayException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/**
+ * Flyway配置类
+ *
+ * @author fulsun
+ */
+@Configuration
+@Slf4j
+public class FlywayConfig {
+    @Autowired
+    private DataSource dataSource;
+
+    /**
+     * 保证Flyway在DataSource初始化之后执行
+     *
+     * @return Flyway对象
+     */
+
+    @PostConstruct
+    public Flyway flyway() {
+        try {
+            Flyway flyway = Flyway.configure().dataSource(dataSource).cleanDisabled(true).baselineOnMigrate(true).baselineVersion("0").locations("db/migration").load();
+            flyway.migrate();
+            log.info("flyway migrated successfully");
+            return flyway;
+        } catch (FlywayException e) {
+            log.error("Flyway 配置加载出错", e);
+            throw new RuntimeException(e);
+        }
+    }
+}
