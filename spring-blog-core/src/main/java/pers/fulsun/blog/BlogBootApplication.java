21f080ff55d89cc2f6d7cabcfbc17bf441f4f301
fulsun <fl.yi6145@gmail.com>
2024-12-22 18:09:49 +0800

    新增博客安装页面

--- spring-blog-core/src/main/java/pers/fulsun/blog/BlogBootApplication.java
+++ spring-blog-core/src/main/java/pers/fulsun/blog/BlogBootApplication.java
@@ -5,16 +5,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
-import tk.mybatis.spring.annotation.MapperScan;
 
 @Slf4j
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-@MapperScan("pers.fulsun.blog.web.admin.mapper")
 public class BlogBootApplication {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(BlogBootApplication.class, args);
         log.info("访问地址：http://127.0.0.1:{}/", context.getEnvironment().getProperty("server.port"));
+        log.info("安装博客地址：http://127.0.0.1:{}/admin/install.html", context.getEnvironment().getProperty("server.port"));
         log.info("后台登录地址：http://127.0.0.1:{}/admin/login.html", context.getEnvironment().getProperty("server.port"));
     }
 }
