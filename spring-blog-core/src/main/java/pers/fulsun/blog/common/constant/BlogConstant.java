ebf99349606fc45d591cd0764a73e036daf6bc73
fulsun <fl.yi6145@gmail.com>
2024-12-24 22:18:41 +0800

    添加默认文章封面常量，并新增页面常量类。

---
+++ spring-blog-core/src/main/java/pers/fulsun/blog/common/constant/BlogConstant.java
@@ -0,0 +1,29 @@
+package pers.fulsun.blog.common.constant;
+
+/**
+ * 用户常量类
+ *
+ * @author fulsun
+ */
+public class BlogConstant {
+    /**
+     * 默认图片目录
+     */
+    public static final String DEFAULT_IMG_DIR = "/admin/assets/custom/images";
+    /**
+     * 默认用户头像
+     */
+    public static final String DEFAULT_AVATAR = DEFAULT_IMG_DIR + "/avatar/default_avatar.jpg";
+
+    /**
+     * 默认分类图片
+     */
+    public static final String DEFAULT_CATEGORY_COVER = DEFAULT_IMG_DIR + "/category/default_category.jpg";
+
+    /**
+     * 默认文章封面
+     */
+    public static final String DEFAULT_POST_COVER = DEFAULT_IMG_DIR + "/post/default_post.jpg";
+
+}
+
---
+++ spring-blog-core/src/main/java/pers/fulsun/blog/common/constant/PageConstant.java
@@ -0,0 +1,12 @@
+package pers.fulsun.blog.common.constant;
+
+/**
+ * 页面常量类
+ * @author fulsun
+ */
+public class PageConstant {
+    /**
+     * md 转 html 的 key
+     */
+    public static final String MARKDOWN_KEY = "markdown:key";
+}
